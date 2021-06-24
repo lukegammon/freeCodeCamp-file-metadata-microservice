e97af74583307459d60cefb93e77b1794e5ddf78
lukegammon <54638816+lukegammon@users.noreply.github.com>
2021-06-24 16:08:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -28,9 +28,6 @@ app.post("/api/fileanalyse", (req, res, next) => {
   })
 })
 
-
-
-
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
